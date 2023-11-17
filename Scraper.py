a6989b20972f0e1fe88b1beb397e895825d86456
Ethan Gelinas <115745794+WritingHusky@users.noreply.github.com>
2023-11-17 09:43:50 -0800

    Clean Up

--- Scraper.py
+++ Scraper.py
@@ -1,50 +1,42 @@
 import tkinter
 from tkinter import filedialog
 from bs4 import BeautifulSoup
-import pandas as pd
 import csv
 
 root = tkinter.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()
 
-assert file_path.endswith(".html"), "Chossen file is not an html file"
-HTMLFile = open(file_path,"r")
+assert file_path.endswith(".html"), "Chosen file is not an HTML file"
+HTMLFile = open(file_path, "r")
 
 text = HTMLFile.read()
 
 soup = BeautifulSoup(text, "html.parser")
 
-fullTable = soup.find_all("th",{"class": "ddtitle"})
-titleList = []
-for title in fullTable:
-    header = title.a.text.split('-')
-    header.pop(0)
-    singleHeader = []
-    for thing in header:
-        singleHeader.append(thing) 
-    titleList.append(singleHeader)  
-
-scheduleTables = soup.find_all("table",{"class": "datadisplaytable", "summary":"This table lists the scheduled meeting times and assigned instructors for this class.."})
+scheduleTables = soup.find_all("table", {"class": "datadisplaytable", "summary": "This table lists the scheduled meeting times and assigned instructors for this class.."})
 
 classesList = []
-header = ["Type","Location","Date Range", "Schedule Type", "Instructor","Start", "End","CRN","Course","Section","Days"]
+header = ["Type", "Location", "Date Range", "Schedule Type", "Instructor", "Start", "End", "CRN", "Course", "Section", "Days"]
 # Need: crn, course, section, split times
-# Loop through each table 
-for table in scheduleTables:
-    
+# Loop through each table and associate title information
+for table, title in zip(scheduleTables, soup.find_all("th", {"class": "ddtitle"})):
+
     # find the data
-    tableData = table.find_all("td",{"class": "dddefault"})
-    classInfo =[]
+    tableData = table.find_all("td", {"class": "dddefault"})
+    classInfo = []
+    
     for info in tableData:
-        if issubclass(type(info.text),str):
-            classInfo.append(str(info.text))
+        if isinstance(info.text, str):
+            classInfo.append(info.text)
         else:
             classInfo.append("TBA")
+    
     # "Type","Time","Days","Location","Date Range", "Schedule Type", "Instructor"
     if "(P)" in classInfo[6]:
-        classInfo[6] = classInfo[6].replace("(P)","")
-    #  Split the times
+        classInfo[6] = classInfo[6].replace("(P)", "")
+    
+    # Split the times
     if '-' in classInfo[1]:
         times = classInfo.pop(1).split('-')
         classInfo.append(times[0])
@@ -53,37 +45,28 @@ for table in scheduleTables:
         info = classInfo.pop(1)
         classInfo.append(info)
         classInfo.append(info)
-        
     
     # Add title data
-    for titleInfo in titleList.pop(0):
-        classInfo.append(titleInfo)
-        
+    header = title.a.text.split('-')
+    header.pop(0)
+    for thing in header:
+        classInfo.append(thing)
+
     # repeat if days repeat
     days = classInfo.pop(1).strip()
     if days != "TBA":
-        # print(days)
-        length = len(days)
-        # print(length)
         for day in days:
-            # print(i)
             newList = classInfo.copy()
-            # print(newList)
             newList.append(day)
-            # print(newList)
             classesList.append(newList)
-            # print("AHHH ----")
-        
+
 # print(classesList)
 
 fileName = "Schedule.csv"
 
 with open(fileName, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    
+
     writer.writerow(header)
     for row in classesList:
-        # print(row)
         writer.writerow(row)
-    # writer.writerows(classesList)
-    
